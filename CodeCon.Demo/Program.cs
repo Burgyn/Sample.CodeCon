2290d8e98af24c51f7f55d2f2a8efea007a51ea2
Milan Martiniak <mino.martiniak@gmail.com>
2022-04-21 07:43:00 +0200

    Init

---
+++ CodeCon.Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CodeCon.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address() { City = "Žilina", Street = "Kvačalová" };
+            Console.WriteLine(address);
+        }
+    }
+
+    [ToString]
+    public partial class Address
+    {
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string Country { get; set; }
+    }
+}
